5e8ddfefe91a4d78a01c755c1ff9269ccfbfcf3b
fariasdavi1999 <fariasdavi1999@gmail.com>
2024-07-30 14:49:30 -0300

    Fix: remove import

--- src/main/java/edu/davi/list/sorting/PeopleSorting.java
+++ src/main/java/edu/davi/list/sorting/PeopleSorting.java
@@ -2,7 +2,6 @@ package main.java.edu.davi.list.sorting;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 public class PeopleSorting {
